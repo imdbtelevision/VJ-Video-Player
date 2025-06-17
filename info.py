5f956d343e0ad2a3b81b0a97b9eeaf7c3fff5c44
imdbtelevision <161870953+imdbtelevision@users.noreply.github.com>
2025-06-17 21:05:26 +0400

    Update info.py

--- info.py
+++ info.py
@@ -9,22 +9,22 @@ API_ID = int(environ.get('API_ID', '28217539'))
 API_HASH = environ.get('API_HASH', 'bc8b8ac22abd8b6e8b8c994f673fc1bd')
 
 # Bot Token, This Is Main Bot
-BOT_TOKEN = environ.get('BOT_TOKEN', "7891831944:AAEcu_zfMjmpknfR6HpJoZ0vCM3iBBW0-gQ")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Admin Telegram Account Id For Withdraw Notification Or Anything Else
 ADMIN = int(environ.get('ADMIN', '1133949754'))
 
 # Back Up Bot Token For Fetching Message When Floodwait Comes
-BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "7891831944:AAEcu_zfMjmpknfR6HpJoZ0vCM3iBBW0-gQ")
+BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
 
 # Log Channel, In This Channel Your All File Stored.
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002137297543'))
 
 # Mongodb Database For User Link Click Count Etc Data Store.
-MONGODB_URI = environ.get("MONGODB_URI", "mongodb+srv://imdbtelevision51477:imdbtv51477@cluster0.9gt0q0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGODB_URI = environ.get("MONGODB_URI", "")
 
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
-STREAM_URL = environ.get("STREAM_URL", "https://frequent-biddy-imdb-television-5cd68e3d.koyeb.app/")
+STREAM_URL = environ.get("STREAM_URL", "")
 
 # This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
 LINK_URL = environ.get("LINK_URL", "https://imdb---television.blogspot.com/2025/06/imdb.html?")
